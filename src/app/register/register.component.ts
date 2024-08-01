a549467eb6a797cf693ffec0d262a709ed5ec994
Ishini Ranasinghe <124230210+IshiniRanasinghe@users.noreply.github.com>
2024-08-01 12:59:43 +0530

    Updated-registerComponent

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -15,47 +15,77 @@ export class RegisterComponent {
     email: '',
     password: ''
   };
-
   constructor(private authService: AuthService, private router: Router) { }
 
   onRegister() {
-    if (this.registerObj.username && this.registerObj.email && this.registerObj.password) {
-      console.log('Attempting to register with:', this.registerObj);
-      this.authService.register(this.registerObj).subscribe(
-        (response) => {
-          if (response) {
-            console.log('Registration successful', response);
-            Swal.fire({
-              icon: 'success',
-              title: 'Registration successful!',
-              text: 'Please login.',
-            }).then(() => {
-              this.router.navigate(['login']);
-            });
-          } else {
-            console.error('Registration failed: No response from server');
-            Swal.fire({
-              icon: 'error',
-              title: 'Registration failed',
-              text: 'Please try with diffrent email or username',
-            });
-          }
-        },
-        (error) => {
-          console.error('Registration failed', error);
-          Swal.fire({
-            icon: 'error',
-            title: 'Registration failed',
-            text: 'Please try again.',
-          });
-        }
-      );
-    } else {
+    if (!this.registerObj.username || !this.registerObj.email || !this.registerObj.password) {
       Swal.fire({
         icon: 'warning',
         title: 'Incomplete fields',
         text: 'Please fill out all fields.',
       });
+      return;
     }
+
+    if (!this.checkEmail((this.registerObj.email.toString()))) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid email',
+        text: 'Please enter a valid email address.',
+      });
+      return;
+    }
+
+    if (!this.passwordCheck()) {
+      return;
+    }
+
+    this.authService.register(this.registerObj).subscribe(
+      (response) => {
+        if (response) {
+          console.log('Registration successful', response);       
+          Swal.fire({
+            icon: 'success',
+            title: 'Registration successful!',
+            text: 'Please login.',
+          }).then(() => {
+            this.router.navigate(['login']);
+          });
+        } else {
+          console.error('Registration failed: No response from server');
+          Swal.fire({
+            icon: 'error',
+            title: 'Registration failed',
+            text: 'Please try with different email or username',
+          });
+        }
+      },
+      (error) => {
+        console.error('Registration failed', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Registration failed',
+          text: 'Please try again.',
+        });
+      }
+    );
+  }
+
+  passwordCheck(): boolean {
+    if (this.registerObj.password.length > 5) {
+      return true;
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid password',
+        text: 'Password must be at least 6 characters long.',
+      });  
+      return false;
+    }
+  }
+
+  checkEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
 }
